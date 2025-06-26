58b3785f1b68fd4cf0dd3c9a666e64f44b7448b1
jonathan YHWH <alphajorider@gmail.com>
2025-06-26 10:54:00 +0200

    types

---
+++ Les_variables/types.py
@@ -0,0 +1,10 @@
+a = 42 # int
+b = 3.14 # float
+c = "Hello" # str
+d = [1, 2, 3] # list
+e = (4, 5, 6) # tuple
+f = {"nom": "Alice", "age": 25} # dict
+g = {1, 2, 3} # set
+h = True # bool
+
+print(a)
