c62b8d78a6b4f6d6d99f98a7fdb3d4d00c9d4ae7
jonathan YHWH <alphajorider@gmail.com>
2025-06-26 16:39:55 +0200

    déclaration variable

--- Les_variables/declaration_variable.py
+++ Les_variables/declaration_variable.py
@@ -1,4 +1,10 @@
-# -*- coding: utf-8 -*-
-x = 10 # Variable entière
-y = 3.14 # Variable flottante
-nom = "Alice" # Variable chaîne de caractères
+a = 42 # int
+b = 3.41 # float
+c = "Hello" # str
+d = [1,2,3] # list
+e = (4,5,6) # tuple
+f = {"nom": "Alice", "age" : 25} # dict
+g = {1,2, 3} # set
+h = True
+j = False
+
