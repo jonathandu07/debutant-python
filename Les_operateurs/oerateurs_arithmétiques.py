00a8fdcd920e8f9669eb7bc61c06ef0fe4858a50
jonathan YHWH <alphajorider@gmail.com>
2025-07-02 17:32:30 +0200

    Create oerateurs_arithmétiques.py

---
+++ Les_operateurs/oerateurs_arithmétiques.py
@@ -0,0 +1,27 @@
+# Les opérateurs
+
+# L'addition
+
+print("Résultat de l'addition : 5 + 3 = ",5+3) # Résultat : 8
+
+print("------------------------------------------------")
+# Soustraction
+print("Résultat de la soustraction : 5 - 3 = ",5-3) # Résultat : 2
+
+print("------------------------------------------------")
+# La multiplication
+print("Résultat de la multiplaction : 5 X 3 = ",5*3) # Résultat : 15
+
+print("------------------------------------------------")
+# La dévision
+print("Résultat de la division : 5 / 3 = ",5/3) # Résultat : 1.6
+
+print("------------------------------------------------")
+# La dévision entière (retourne comnbien de fois on peut diviser le nombre)
+print("Résultat de la division : 5 // 3 = ",5//3) # Résultat : 1
+
+print("------------------------------------------------")
+# La puissance 
+print("Résultat de la puissance : 5 ** 3 = ",5**3) # Résultat : 1
+
+# Les opérateurs de comparaison 
