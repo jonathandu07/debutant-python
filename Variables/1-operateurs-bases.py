86d8e9a5d2af877118622c5c5d8a5e95303094b6
jonathan YHWH <alphajorider@gmail.com>
2025-10-01 10:43:50 +0200

    opération de base

--- Variables/1-operateurs-bases.py
+++ Variables/1-operateurs-bases.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  1 10:32:46 2025
-
-@author: alpha
+Les opérateurs arithmétiques
 """
 
+# Addition
+Addition = 5 + 3
+
+# Soustraction
+Soustraction = 5 - 3
+
+# Multiplication
+Multiplication = 5 * 3
+
+# Division
+Division = 5 / 3
+
+# Division entière : Divise la première valeur par la deuxième et retourne le quotient entier.
+div_entiere = 5 // 3
+
+# Modulo : Donne ce qui reste d'une vision.
+modulo = 5 % 3
+
+# Puissance
+puissance = 5 ** 3
