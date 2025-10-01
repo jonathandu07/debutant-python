8e9801505c6520064529068e4cf59ed55d72db11
jonathan YHWH <alphajorider@gmail.com>
2025-10-01 10:52:06 +0200

    1-operateurs-comparaison

---
+++ Variables/1-operateurs-comparaison.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Les opérateurs de comparaison
+"""
+
+# Vérifier si c'est égale !
+a = 5 == 3 # False !
+z = 5 == 5 # True !
+
+# Différence
+e = 5 != 3
+r = 5 != 5
+
+# Plus grand que 
+t = 5 > 3
+y = 3 > 5
+
+# Plus petit que
+u = 5 < 3
+i = 3 < 5
+
+# Supérieur ou égal
+o = 5 >= 3
+p = 5 >= 5
