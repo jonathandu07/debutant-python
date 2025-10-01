1984aee719f10a203e373801d4cc3e9d6877ba63
jonathan YHWH <alphajorider@gmail.com>
2025-10-01 10:27:00 +0200

    1-les-variables-immuables-mutables-references-copie-destruction

--- Variables/1-les-variables-immuables-mutables-references-copie-destruction..py
+++ Variables/1-les-variables-immuables-mutables-references-copie-destruction..py
@@ -3,3 +3,23 @@
 Variables immuables vs mutables
 """
 
+# Type Immuables :
+a = 10
+a = 20
+
+# Types mutables
+liste = [1, 2, 3]
+#liste[0] = 10
+
+b = liste
+
+#b[0] = 10
+
+import copy
+
+b = copy.deepcopy(liste)
+b[0] = 10
+
+x = 10
+del x
+print(x)
