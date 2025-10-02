f0507b09a4741f45f583e077bc986afe94108926
jonathan YHWH <alphajorider@gmail.com>
2025-10-02 09:40:17 +0200

    1-operateurs-apartenance

--- Variables/1-operateurs-apartenance.py
+++ Variables/1-operateurs-apartenance.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct  2 09:36:17 2025
-
-@author: alpha
+Les opérateurs d’appartenance
 """
 
+a = 'a' in 'apple'
+b = 3 in [1,2,3,4]
+
+C = 'y' not in 'apple'
+
+d = 10 not in [1,2,3,4]
