867b0d61f9d8609516a97786ecdd2cfe83c63996
jonathan YHWH <alphajorider@gmail.com>
2025-10-03 10:58:10 +0200

    3-listes-operations

---
+++ Variables/3-listes-operations.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  3 10:55:04 2025
+
+@author: alpha
+"""
+
---
+++ Variables/3-listes-tranchage.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Tranchage (slicing) des listes
+"""
+# sous_liste = ma_liste[start:stop:step]
+nombres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(nombres[2:6])
+print(nombres[0:6:2])
+print(nombres[-3:])
+print(nombres[:])
+print(nombres[::-1])
