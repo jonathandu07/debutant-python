1f17bb32091a6110da923ad33735ee74a7f174d7
jonathan YHWH <alphajorider@gmail.com>
2025-10-06 10:48:37 +0200

    3-listes-operations

--- Variables/3-listes-operations.py
+++ Variables/3-listes-operations.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  3 10:55:04 2025
-
-@author: alpha
+Opérations sur les listes
 """
+# Concaténation
+liste1 = [1, 2, 3]
+liste2 = [4, 5, 6]
+liste_concatenee = liste1 + liste2
+print(liste_concatenee)
+
+print("------------------------------------------")
+# Répétition
+liste_repete = [1, 2, 3] * 3
+print(liste_repete)
+
+print("------------------------------------------")
+# Vérification d'appartenance
+fruits = ["pomme", "banane", "cerise"]
+
+print("banane" in fruits)
+print("orange" in fruits)
 
