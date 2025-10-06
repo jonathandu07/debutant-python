686ec7c0e1a4ac603199eb5498ea19b24c2385de
jonathan YHWH <alphajorider@gmail.com>
2025-10-06 15:35:41 +0200

    4-tuples-methodes

---
+++ Variables/4-les-tuples.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Les tuples
+"""
+
+mon_tuple = ()
+
+mon_tuple = (1, 2, 3)
+
+mon_tuple = (1, "pomme", 3.14)
+
+print(mon_tuple[0])
+print(mon_tuple[-1])
+
+tuple_imbrique = (1, (2, 3), 4)
+print(tuple_imbrique[1][1])
---
+++ Variables/4-tuples-methodes.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  6 15:27:05 2025
+
+@author: alpha
+"""
+
