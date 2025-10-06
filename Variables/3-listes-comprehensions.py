6b128170dbcbb0e2dbfcf4cbca0561a0da5158a8
jonathan YHWH <alphajorider@gmail.com>
2025-10-06 11:07:52 +0200

    Create 3-listes-comprehensions.py

---
+++ Variables/3-listes-comprehensions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Parcourir une liste
+"""
+
+fruits = ["pomme", "banane", "cerise"]
+
+for fruit in fruits:
+    print(fruit)
+    
+# liste des carrés de 0 à 9
+carres = [x**2 for x in range(10)]
+print(carres)
