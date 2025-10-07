b858446adf971ac65d55a9a664879210653bb5b4
jonathan YHWH <alphajorider@gmail.com>
2025-10-07 17:04:40 +0200

    Update 4-tuples-iteration.py

--- Tuples/4-tuples-iteration.py
+++ Tuples/4-tuples-iteration.py
@@ -1,7 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  7 11:27:55 2025
-
-@author: alpha
+Itération et utilisation avancée des tuples
 """
+
+mon_tuple = ("pomme", "banane", "cerise")
+for fruit in mon_tuple:
+    print(fruit)
+    
+mon_tuple = (1, 2, 3)
+a, b, c = mon_tuple
+print(a, b, c)
+
+tuples_list = [(1, 'a'), (2, 'b')]
+for number, letter in tuples_list:
+    print(number, letter)
+    
+mon_dict = {(1, 2): "point A", (3, 4): "Point B"}
+print(mon_dict[1, 2])
+
+ma_liste = [1, 2, 3]
+mon_tuple = tuple(ma_liste)
+print(mon_tuple)
+
+from collections import namedtuple
+
+Point = namedtuple("Point", ["x", "y"])
+p = Point(10, 20)
+print(p.x, p.y)
+
+
+
+
+
+
+
+
 
