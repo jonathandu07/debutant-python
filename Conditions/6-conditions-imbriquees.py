22553f53585c0b358f02265c4fdbf523f2d150c7
jonathan YHWH <alphajorider@gmail.com>
2025-10-09 15:05:08 +0200

    6-conditions-avancees

--- Conditions/6-conditions-imbriquees.py
+++ Conditions/6-conditions-imbriquees.py
@@ -18,3 +18,11 @@ else:
 age = 18
 message = "Majeur" if age >= 18 else "Mineur"
 print(message)
+
+age = 25
+revenu_annuel = 50000
+
+if age > 18 and revenu_annuel > 30000:
+    print("Vous êtes éligible à un prêt.")
+else:
+    print("Vous n'êtes pas éligible à un prêt.")
