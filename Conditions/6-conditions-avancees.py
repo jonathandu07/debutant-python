bc66a35ffb49f2e3e9eec04755df2b19ffa0acd6
jonathan YHWH <alphajorider@gmail.com>
2025-10-09 15:56:19 +0200

    Update 6-conditions-avancees.py

--- Conditions/6-conditions-avancees.py
+++ Conditions/6-conditions-avancees.py
@@ -1,7 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct  9 15:10:48 2025
-
-@author: alpha
+Conditions avancées : pass , structures de données et match
 """
 
+age = 20
+
+if age >= 18:
+    pass
+else:
+    print("Vous êtes mineur !")
+    
+fruits = ["pomme", "banane", "cerise"]
+
+if "pomme" in fruits:
+    print("Il y a des pommes")
+else:
+    print("il n'y a pas de pommes.")
+    
+    
+choix = 1
+
+match choix:
+    case 1:
+        print("Nouveau fichier")
+    case 2:
+        print("Ouvrir un fichier")
+    case 3:
+        print("Sauvegarder")
+    case _:
+        print("Option inconue")
