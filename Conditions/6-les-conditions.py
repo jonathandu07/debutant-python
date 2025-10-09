694d158835356ed341347445b47857e40d357fea
jonathan YHWH <alphajorider@gmail.com>
2025-10-09 14:46:19 +0200

    6-les-conditions

--- Conditions/6-les-conditions.py
+++ Conditions/6-les-conditions.py
@@ -1,7 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct  9 14:29:26 2025
-
-@author: alpha
+Les conditions en Python
 """
 
+age = 18
+
+if age >= 18:
+    print("Vous êtes majeur !")
+    
+    
+print("--------------------------------------------------------")
+
+age = 16
+if age >= 18:
+    print("Vous êtes majeur !")
+else :
+    print("Vous êtes mineur !")
+    
+print("--------------------------------------------------------")
+
+note = 85
+
+if note >= 90:
+    print("Excellent !")
+elif note >= 80:
+    print("Très bien !")
+elif note >= 70:
+    print("Bien !")
+else :
+    print("Vous pouvez mieux faire ! :-(")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
