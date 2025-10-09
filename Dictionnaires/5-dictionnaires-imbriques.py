0dbe51c1730462c293cd4e65fe810033f3f4919f
jonathan YHWH <alphajorider@gmail.com>
2025-10-09 13:09:28 +0200

    Update 5-dictionnaires-imbriques.py

--- Dictionnaires/5-dictionnaires-imbriques.py
+++ Dictionnaires/5-dictionnaires-imbriques.py
@@ -3,3 +3,23 @@
 Dictionnaires imbriqués et avancés
 """
 
+etudiant = {
+    "nom" : "Alice",
+    "âge" : 25,
+    "cours" : {
+        "math" : 85,
+        "physique" : 90,
+        "informatique" : 95
+        }
+    }
+
+print(etudiant["cours"]["math"])
+
+liste_tuples = [("nom", "Alice"), ("âge", 25), ("Ville", "Paris")]
+mon_dict = dict(liste_tuples)
+print(mon_dict)
+
+cles = ["nom", "âge", "ville"]
+valeurs = ["Alice", 25, "Paris"]
+monDict = dict(zip(cles, valeurs))
+print(monDict)
