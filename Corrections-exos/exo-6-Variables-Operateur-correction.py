4afafe200a31217aa838c8fc4667fa27c261974d
jonathan YHWH <alphajorider@gmail.com>
2025-12-08 16:06:00 +0100

    Create exo-6-Variables-Operateur-correction.py

---
+++ Corrections-exos/exo-6-Variables-Operateur-correction.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Déclaration des variables
+longueur = 7
+largeur = 4
+
+# Calcul du périmètre
+perimetre = 2 * (longueur + largeur)
+
+# Affichage du résultat
+print("Le périmètre du rectangle est :", perimetre)  # Résultat : 22
