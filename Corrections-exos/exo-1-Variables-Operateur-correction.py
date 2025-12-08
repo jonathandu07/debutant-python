2a7c46b0636bc72fb7d19c67c38ef1afbf47eb51
jonathan YHWH <alphajorider@gmail.com>
2025-12-08 11:25:41 +0100

    Create exo-1-Variables-Operateur-correction.py

---
+++ Corrections-exos/exo-1-Variables-Operateur-correction.py
@@ -0,0 +1,23 @@
+# Déclaration des variables
+a = 12
+b = 8
+
+# Addition
+addition = a + b
+print("Addition:", addition)  # Résultat : 20
+
+# Soustraction
+soustraction = a - b
+print("Soustraction:", soustraction)  # Résultat : 4
+
+# Multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)  # Résultat : 96
+
+# Division
+division = a / b
+print("Division:", division)  # Résultat : 1.5
+
+# Modulo
+modulo = a % b
+print("Modulo:", modulo)  # Résultat : 4
