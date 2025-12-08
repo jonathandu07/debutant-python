f25184b98ca136a865f9bf3fdfd0dbf6c97b1dcd
jonathan YHWH <alphajorider@gmail.com>
2025-12-08 14:56:09 +0100

    Create exo-5-Variables-Operateur-correction.py

---
+++ Corrections-exos/exo-5-Variables-Operateur-correction.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Déclaration de la variable
+z = 10
+
+# Ajouter 5 à z
+z += 5
+print("Après addition de 5 :", z)  # Résultat : 15
+
+# Multiplier z par 2
+z *= 2
+print("Après multiplication par 2 :", z)  # Résultat : 30
+
+# Soustraire 4 de z
+z -= 4
+print("Après soustraction de 4 :", z)  # Résultat : 26
+
+# Diviser z par 3
+z /= 3
+print("Après division par 3 :", z)  # Résultat : 8.666666666666666 (ou arrondi 8.67 selon la précision)
