2287e77b94cd9b989727f80eb767604166196b68
jonathan YHWH <alphajorider@gmail.com>
2025-12-16 10:51:17 +0100

    Update exo-7-Variables-Operateur-correction.py

--- Corrections-exos/exo-7-Variables-Operateur-correction.py
+++ Corrections-exos/exo-7-Variables-Operateur-correction.py
@@ -1,1 +1,10 @@
-# -*- coding: utf-8 -*-
+# Déclaration des variables
+a = 10
+b = 20
+
+# Échange des valeurs sans variable temporaire
+a, b = b, a
+
+# Affichage des nouvelles valeurs
+print("Nouvelle valeur de a :", a)  # Résultat : 20
+print("Nouvelle valeur de b :", b)  # Résultat : 10
