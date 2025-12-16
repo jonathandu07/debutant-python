e7ff5f5893e2e7d0945593390fa8ec1e18691235
jonathan YHWH <alphajorider@gmail.com>
2025-12-16 10:58:48 +0100

    Create exo-9-Variables-Operateur-correction.py

---
+++ Corrections-exos/exo-9-Variables-Operateur-correction.py
@@ -0,0 +1,9 @@
+# Déclaration des variables
+x = [1, 2, 3]
+y = [1, 2, 3]
+
+# Vérification si x et y sont le même objet en mémoire
+sont_meme_objet = x is y
+
+# Affichage du résultat
+print("x et y sont le même objet en mémoire :", sont_meme_objet)  # Résultat : False
