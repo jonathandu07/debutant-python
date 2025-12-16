6ae26783fdf63a260205dbde370249b244ebe4c0
jonathan YHWH <alphajorider@gmail.com>
2025-12-16 10:55:16 +0100

    Create exo-8-Variables-Operateur-correction.py

---
+++ Corrections-exos/exo-8-Variables-Operateur-correction.py
@@ -0,0 +1,10 @@
+# Déclaration des variables
+n1 = 14
+n2 = 18
+n3 = 20
+
+# Calcul de la moyenne
+moyenne = (n1 + n2 + n3) / 3
+
+# Affichage du résultat
+print("La moyenne des trois nombres est :", moyenne)  # Résultat : 17.333333333333332 (ou arrondi à 17.33 selon la précision)
