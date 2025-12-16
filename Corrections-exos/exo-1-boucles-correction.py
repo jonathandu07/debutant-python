55bdb2c9f40a8da3747104be9d18694bd16a1bf2
jonathan YHWH <alphajorider@gmail.com>
2025-12-16 11:15:10 +0100

    exo-1-boucles-correction

---
+++ Corrections-exos/exo-1-boucles-correction.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 16 11:13:43 2025
+
+@author: alpha
+"""
+
+for i in range(1, 11):
+    print(i)
---
+++ Corrections-exos/exo-10-Variables-Operateur-correction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 16 11:03:56 2025
+
+@author: alpha
+"""
+
+lettre = "a"
+mot = "python"
+
+est_dans_mot = lettre in mot
+
+print("La lettre est dans le mot :", est_dans_mot)
