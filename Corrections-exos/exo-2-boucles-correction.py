4cc9eb0810b5985435fd490e0a211188e5bb1ba7
jonathan YHWH <alphajorider@gmail.com>
2025-12-17 09:44:17 +0100

    Create exo-2-boucles-correction.py

---
+++ Corrections-exos/exo-2-boucles-correction.py
@@ -0,0 +1,9 @@
+# Initialisation de la variable pour stocker la somme
+somme = 0
+
+# Boucle for pour additionner les nombres de 1 à 100
+for i in range(1, 101):
+    somme += i  # Ajout de chaque nombre à la somme
+
+# Affichage du résultat
+print("La somme des nombres de 1 à 100 est :", somme)
