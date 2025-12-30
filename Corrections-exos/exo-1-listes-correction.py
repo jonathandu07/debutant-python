892218fde0b0e56b6213027dbd5d8bb3665d8a05
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 17:12:43 +0100

    exo 2

--- Corrections-exos/exo-1-listes-correction.py
+++ Corrections-exos/exo-1-listes-correction.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
+# Création de la liste
 animaux = ["chat", "chien", "souris", "éléphant"]
 
+# Accès au premier élément (indice 0)
 premier_animal = animaux[0]
-print("Premier animal :", premier_animal)
+print("Premier animal :", premier_animal)  # Résultat : "chat"
 
+# Accès au dernier élément (indice -1)
 dernier_animal = animaux[-1]
-print("Dernier animal : ", dernier_animal)
+print("Dernier animal :", dernier_animal)  # Résultat : "éléphant"
---
+++ Corrections-exos/exo-2-listes-correction.py
@@ -0,0 +1,8 @@
+# Création de la liste initiale (si elle n'est pas déjà créée)
+animaux = ["chat", "chien", "souris", "éléphant"]
+
+# Remplacement de "souris" par "lion"
+animaux[2] = "lion"  # L'élément "souris" est à l'indice 2
+
+# Affichage de la liste modifiée
+print("Liste modifiée :", animaux)
