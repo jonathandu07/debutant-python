2a4177e4fa62c3b8e56892ef1c560fe484572c3b
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 18:09:21 +0100

    Create exo-7-listes-correction.py

---
+++ Corrections-exos/exo-7-listes-correction.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 30 17:49:08 2025
+
+@author: alpha
+"""
+
+melange = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+liste_triee = sorted(melange)
+
+melange.reverse()
