c6fba9611f254a1b988a37ac1e9d642a6420942a
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 18:10:18 +0100

    Update exo-7-listes-correction.py

--- Corrections-exos/exo-7-listes-correction.py
+++ Corrections-exos/exo-7-listes-correction.py
@@ -1,12 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 30 17:49:08 2025
-
-@author: alpha
-"""
-
+# Création de la liste mélange
 melange = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 
+# Utilisation de sorted() pour trier la liste
 liste_triee = sorted(melange)
 
+# Affichage de la liste triée
+print("Liste triée :", liste_triee)
+
+# Inversion de l'ordre des éléments de la liste originale en utilisant reverse()
 melange.reverse()
+
+# Affichage de la liste inversée
+print("Liste originale inversée :", melange)
