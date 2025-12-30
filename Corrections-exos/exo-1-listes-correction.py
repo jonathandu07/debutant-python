aa661b96f6ef1268d0771b7b58db1635cc6b8c05
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 17:10:14 +0100

    Update exo-1-listes-correction.py

--- Corrections-exos/exo-1-listes-correction.py
+++ Corrections-exos/exo-1-listes-correction.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+animaux = ["chat", "chien", "souris", "éléphant"]
+
+premier_animal = animaux[0]
+print("Premier animal :", premier_animal)
+
+dernier_animal = animaux[-1]
+print("Dernier animal : ", dernier_animal)
