9740ae0cb560fb1ed81cb7548cab28491da1d18d
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 17:15:28 +0100

    exo 3

---
+++ Corrections-exos/exo-3-listes-correction.py
@@ -0,0 +1,17 @@
+# Création de la liste vide
+fruits = []
+
+# Ajout de "pomme" à la fin de la liste en utilisant append()
+fruits.append("pomme")
+
+# Ajout de "banane" à la fin de la liste en utilisant append()
+fruits.append("banane")
+
+# Ajout de "cerise" à la fin de la liste en utilisant append()
+fruits.append("cerise")
+
+# Ajout de "orange" en deuxième position (indice 1) en utilisant insert()
+fruits.insert(1, "orange")
+
+# Affichage de la liste finale
+print("Liste finale des fruits :", fruits)
