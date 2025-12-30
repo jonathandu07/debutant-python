7b25ef77b813d6fcb27f87fd8b78b1df98b9ae7d
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 17:41:17 +0100

    Create exo-4-listes-correction.py

---
+++ Corrections-exos/exo-4-listes-correction.py
@@ -0,0 +1,12 @@
+# Liste fruits créée précédemment
+fruits = ['pomme', 'orange', 'banane', 'cerise']
+
+# Suppression de "banane" en utilisant remove()
+fruits.remove("banane")
+
+# Suppression et affichage du dernier élément en utilisant pop()
+dernier_fruit = fruits.pop()
+print("Dernier fruit retiré :", dernier_fruit)
+
+# Affichage de la liste restante
+print("Liste restante des fruits :", fruits)
