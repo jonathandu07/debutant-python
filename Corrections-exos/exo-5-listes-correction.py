38732af4cd986ee07a62da8b2c1b3a4f934a8387
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 17:43:54 +0100

    Create exo-5-listes-correction.py

---
+++ Corrections-exos/exo-5-listes-correction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 30 17:41:19 2025
+
+@author: alpha
+"""
+
+liste1 = [1, 2, 3]
+liste2 = [4, 5, 6]
+
+liste3 = liste1 + liste2
+
+liste4 = liste3 * 3
