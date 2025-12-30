90e6dae8f6e6efed241fba8dc1e1514e393f5104
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 19:14:29 +0100

    Create exo-8-listes-correction.py

---
+++ Corrections-exos/exo-8-listes-correction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 30 19:10:49 2025
+
+@author: alpha
+"""
+
+voyelles = ["a", "o", "u", "e", "y", "i"]
+
+for index, voyelle in enumerate(voyelles):
+    print(f"indice {index} : {voyelle}")
