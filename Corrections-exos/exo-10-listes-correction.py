a3d1c1b7208c96b872c38f1b65659100f38399d8
jonathan YHWH <alphajorider@gmail.com>
2025-12-30 20:23:37 +0100

    Create exo-10-listes-correction.py

---
+++ Corrections-exos/exo-10-listes-correction.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 30 20:20:43 2025
+
+@author: alpha
+"""
+
+nombres = list(range(10))
+
+elements_2_5 = nombres[2:6]
+
+liste_inversee = nombres[::-1]
