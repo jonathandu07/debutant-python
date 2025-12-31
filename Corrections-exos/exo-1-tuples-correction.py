fd57858209728ca120be96e7255d56bbff8a51c1
jonathan YHWH <alphajorider@gmail.com>
2025-12-31 14:49:27 +0100

    Create exo-1-tuples-correction.py

---
+++ Corrections-exos/exo-1-tuples-correction.py
@@ -0,0 +1,10 @@
+# Création du tuple coordonnées
+coordonnees = (10, 20)
+
+# Accès au premier élément du tuple
+x = coordonnees[0]
+print("Premier élément (x) :", x)
+
+# Accès au deuxième élément du tuple
+y = coordonnees[1]
+print("Deuxième élément (y) :", y)
