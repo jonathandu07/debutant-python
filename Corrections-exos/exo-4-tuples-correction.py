fbb6e7cb5e414aa88a9dc46750561b94c908876d
jonathan YHWH <alphajorider@gmail.com>
2025-12-31 15:01:04 +0100

    Create exo-4-tuples-correction.py

---
+++ Corrections-exos/exo-4-tuples-correction.py
@@ -0,0 +1,8 @@
+# Création de la liste nombres
+nombres = [4, 5, 6]
+
+# Conversion de la liste en tuple
+nombres_tuple = tuple(nombres)
+
+# Affichage du résultat
+print("Liste convertie en tuple :", nombres_tuple)
