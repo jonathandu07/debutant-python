f61eafe1bcd97e1774666b2ff3fe60044b0caae3
jonathan YHWH <alphajorider@gmail.com>
2025-12-31 15:30:31 +0100

    Create exo-7-tuples-correction.py

---
+++ Corrections-exos/exo-7-tuples-correction.py
@@ -0,0 +1,9 @@
+# Création du tuple dimensions
+dimensions = (1920, 1080)
+
+# Déballage du tuple pour attribuer les valeurs à largeur et hauteur
+largeur, hauteur = dimensions
+
+# Affichage des variables largeur et hauteur
+print("Largeur :", largeur)
+print("Hauteur :", hauteur)
