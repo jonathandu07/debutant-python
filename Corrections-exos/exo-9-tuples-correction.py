cd35a6aa5fd629703bfb9075b51339912d3a50eb
jonathan YHWH <alphajorider@gmail.com>
2026-01-01 14:43:59 +0100

    Update exo-9-tuples-correction.py

--- Corrections-exos/exo-9-tuples-correction.py
+++ Corrections-exos/exo-9-tuples-correction.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  1 14:04:26 2026
+import time
 
-@author: alpha
-"""
+liste = list(range(1, 1000000001))
 
-liste = list(range(1, 1000001))
-tuple_ = tuple(range(1, 1000001))
+tuple_ = tuple(range(1, 1000000001))
 
 milieu = len(liste) // 2
+
+debut_liste = time.time()
+element_liste = liste[milieu]
+fin_liste = time.time()
+
+debut_tuple = time.time()
+element_tuple = liste[milieu]
+fin_tuple = time.time()
+
+temps_liste = fin_liste - debut_liste
+
+temps_tuple = fin_tuple - debut_tuple
+
+print(f"Temps d'accès à l'élément du milieu de la liste : {temps_liste}")
+
+print(f"Temps d'accès à l'élément du milieu du tuple : {temps_tuple}")
