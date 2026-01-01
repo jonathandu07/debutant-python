32f604fa223d38ec364287ff42059a8fe81d4a37
jonathan YHWH <alphajorider@gmail.com>
2026-01-01 15:55:07 +0100

    Create exo-2-dictionnaire-correction.py

---
+++ Corrections-exos/exo-2-dictionnaire-correction.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  1 15:22:45 2026
+
+@author: alpha
+"""
+
+étudiant = {
+    "nom": "Jean",
+    "âge": 22,
+    "cours": ["Math", "Physique", "Informatique"]
+}
+
+étudiant["âge"] = 23
+
+étudiant["cours"].append("chimie")
