17128874b43a1935f168c74a7fc5b6451ae3d936
jonathan YHWH <alphajorider@gmail.com>
2026-01-01 15:23:03 +0100

    Create exo-1-dictionnaire-correction.py

---
+++ Corrections-exos/exo-1-dictionnaire-correction.py
@@ -0,0 +1,15 @@
+# Dictionnaire étudiant initial
+étudiant = {
+    "nom": "Jean",
+    "âge": 22,
+    "cours": ["Math", "Physique", "Informatique"]
+}
+
+# Modification de l'âge de l'étudiant
+étudiant["âge"] = 23
+
+# Ajout d'un nouveau cours à la liste des cours
+étudiant["cours"].append("Chimie")
+
+# Affichage du dictionnaire mis à jour
+print("Dictionnaire étudiant mis à jour :", étudiant)
