65702d4975184c4bfe691cb9457d2b8c8bd4bed4
jonathan YHWH <alphajorider@gmail.com>
2026-01-01 14:11:36 +0100

    correction

---
+++ Corrections-exos/exo-8-tuples-correction.py
@@ -0,0 +1,6 @@
+# Création du tuple couleurs
+couleurs = ("rouge", "vert", "bleu")
+
+# Utilisation d'une boucle for pour parcourir et afficher chaque élément du tuple
+for couleur in couleurs:
+    print(couleur)
---
+++ Corrections-exos/exo-9-tuples-correction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  1 14:04:26 2026
+
+@author: alpha
+"""
+
+liste = list(range(1, 1000001))
+tuple_ = tuple(range(1, 1000001))
+
+milieu = len(liste) // 2
