6be2f3246a9b196881f509098d636c342c23d4f7
jonathan YHWH <alphajorider@gmail.com>
2026-01-01 15:55:44 +0100

    Update exo-2-dictionnaire-correction.py

--- Corrections-exos/exo-2-dictionnaire-correction.py
+++ Corrections-exos/exo-2-dictionnaire-correction.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  1 15:22:45 2026
-
-@author: alpha
-"""
-
+# Dictionnaire étudiant initial
 étudiant = {
     "nom": "Jean",
     "âge": 22,
     "cours": ["Math", "Physique", "Informatique"]
 }
 
+# Modification de l'âge de l'étudiant
 étudiant["âge"] = 23
 
-étudiant["cours"].append("chimie")
+# Ajout d'un nouveau cours à la liste des cours
+étudiant["cours"].append("Chimie")
+
+# Affichage du dictionnaire mis à jour
+print("Dictionnaire étudiant mis à jour :", étudiant)
