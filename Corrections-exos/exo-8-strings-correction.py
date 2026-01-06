25d46a724f2443071eb128045c65f6bf9b28ab14
jonathan YHWH <alphajorider@gmail.com>
2026-01-06 17:57:43 +0100

    Create exo-8-strings-correction.py

---
+++ Corrections-exos/exo-8-strings-correction.py
@@ -0,0 +1,8 @@
+# Création de la chaîne mot_de_passe
+mot_de_passe = "motDePasse123"
+
+# Vérification de l'appartenance de "123" dans le mot de passe
+if "123" in mot_de_passe:
+    print("Mot de passe valide")
+else:
+    print("Mot de passe invalide")
