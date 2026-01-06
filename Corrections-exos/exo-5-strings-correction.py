ca90cde5adaa37dc586e367df2098fa8c6e9ac36
jonathan YHWH <alphajorider@gmail.com>
2026-01-06 17:35:19 +0100

    Create exo-5-strings-correction.py

---
+++ Corrections-exos/exo-5-strings-correction.py
@@ -0,0 +1,14 @@
+# Création de la chaîne phrase
+phrase = "  Python est génial!  "
+
+# Suppression des espaces au début et à la fin
+phrase = phrase.strip()
+
+# Conversion de toute la chaîne en majuscules
+phrase = phrase.upper()
+
+# Remplacement de "génial" par "puissant"
+phrase = phrase.replace("GÉNIAL", "PUISSANT")
+
+# Affichage de la chaîne modifiée
+print("Chaîne modifiée :", phrase)
