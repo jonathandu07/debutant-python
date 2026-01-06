3b27cce2b8b3216029e91bb6872d7e03ebf79104
jonathan YHWH <alphajorider@gmail.com>
2026-01-06 16:28:18 +0100

    Create exo-3-strings-correction.py

---
+++ Corrections-exos/exo-3-strings-correction.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  6 16:08:30 2026
+
+@author: alpha
+"""
+
+prenom = "Jean"
+nom = "Dupont"
+
+nom_complet = prenom + " " + nom
+
+print("Nom complet : ", nom_complet)
+
+nom_complet_repetition = nom_complet * 3
+
+print("Nom complet répété 3 fois :", nom_complet_repetition)
+
+
