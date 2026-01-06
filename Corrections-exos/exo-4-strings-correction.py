91243b665322814152cd424639f489c45a1641d7
jonathan YHWH <alphajorider@gmail.com>
2026-01-06 16:30:49 +0100

    Create exo-4-strings-correction.py

---
+++ Corrections-exos/exo-4-strings-correction.py
@@ -0,0 +1,14 @@
+# Création de la chaîne alphabet
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# Accès à la première lettre
+premiere_lettre = alphabet[0]
+print("Première lettre :", premiere_lettre)
+
+# Accès à la dernière lettre
+derniere_lettre = alphabet[-1]
+print("Dernière lettre :", derniere_lettre)
+
+# Accès à la sous-chaîne de la 3ème à la 6ème lettre (indices 2 à 5)
+sous_chaine = alphabet[2:6]
+print("Sous-chaîne de la 3ème à la 6ème lettre :", sous_chaine)
