eb3f7cbed11bf417aaf049fce51b3700747699ca
jonathan YHWH <alphajorider@gmail.com>
2026-01-07 11:53:57 +0100

    correction

---
+++ Corrections-exos/exo-3-conversion-correction.py
@@ -0,0 +1,11 @@
+# Demande à l'utilisateur d'entrer un nombre entier
+nombre_entier = int(input("Entrez un nombre entier : "))
+
+# Affichage du type de la variable avant la conversion
+print("Type avant conversion :", type(nombre_entier))
+
+# Conversion du nombre entier en chaîne de caractères
+nombre_chaine = str(nombre_entier)
+
+# Affichage du type de la variable après la conversion
+print("Type après conversion :", type(nombre_chaine))
---
+++ Corrections-exos/exo-4-conversion-correction.py
@@ -0,0 +1,8 @@
+# Création de la variable chaine avec la valeur "Hello"
+chaine = "Hello"
+
+# Conversion de la variable chaine en booléen
+chaine_booleen = bool(chaine)
+
+# Affichage du résultat
+print(f"La valeur de la chaîne '{chaine}' convertie en booléen est : {chaine_booleen}")
