10b10721c40a63e8f51e66e70aa84c120f069a81
jonathan YHWH <alphajorider@gmail.com>
2026-01-07 14:09:37 +0100

    correction

---
+++ Corrections-exos/exo-5-conversion-correction.py
@@ -0,0 +1,8 @@
+# Demande à l'utilisateur d'entrer une chaîne de caractères
+chaine = input("Entrez une chaîne de caractères : ")
+
+# Conversion de la chaîne en une liste de caractères
+liste_caracteres = list(chaine)
+
+# Affichage du résultat
+print("La chaîne convertie en liste de caractères est :", liste_caracteres)
---
+++ Corrections-exos/exo-6-conversion-correction.py
@@ -0,0 +1,8 @@
+# Création de la liste nombres
+nombres = [1, 2, 3, 4, 5]
+
+# Conversion de la liste en tuple
+nombres_tuple = tuple(nombres)
+
+# Affichage du résultat
+print("La liste convertie en tuple est :", nombres_tuple)
