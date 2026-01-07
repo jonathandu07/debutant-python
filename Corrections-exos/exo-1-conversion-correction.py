8b6b83c0aad3ddcb7c08debea63e70d14d22f925
jonathan YHWH <alphajorider@gmail.com>
2026-01-07 10:35:09 +0100

    Create exo-1-conversion-correction.py

---
+++ Corrections-exos/exo-1-conversion-correction.py
@@ -0,0 +1,8 @@
+# Demande à l'utilisateur d'entrer un nombre flottant
+nombre_flottant = float(input("Entrez un nombre flottant : "))
+
+# Conversion du nombre flottant en entier
+nombre_entier = int(nombre_flottant)
+
+# Affichage du résultat
+print(f"Le nombre {nombre_flottant} converti en entier est : {nombre_entier}")
