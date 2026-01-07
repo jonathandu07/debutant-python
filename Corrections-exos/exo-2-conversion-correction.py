30137a0855650ec9a905b66b62e8a9832d5daf28
jonathan YHWH <alphajorider@gmail.com>
2026-01-07 10:40:58 +0100

    Create exo-2-conversion-correction.py

---
+++ Corrections-exos/exo-2-conversion-correction.py
@@ -0,0 +1,8 @@
+# Création de la variable a avec la valeur 42
+a = 42
+
+# Conversion de la variable a en flottant
+a_flottant = float(a)
+
+# Affichage du résultat
+print(f"La valeur entière {a} convertie en flottant est : {a_flottant}")
