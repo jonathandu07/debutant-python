8e307fed463e4453763988e02d3ea0e9da4fb8ad
jonathan YHWH <alphajorider@gmail.com>
2026-01-08 11:35:19 +0100

    Create exo-3-input-correction.py

---
+++ Corrections-exos/exo-3-input-correction.py
@@ -0,0 +1,11 @@
+# Demande à l'utilisateur d'entrer le premier nombre flottant
+nombre1 = float(input("Entrez le premier nombre flottant : "))
+
+# Demande à l'utilisateur d'entrer le deuxième nombre flottant
+nombre2 = float(input("Entrez le deuxième nombre flottant : "))
+
+# Calcul de la somme des deux nombres
+somme = nombre1 + nombre2
+
+# Affichage du résultat
+print(f"La somme de {nombre1} et {nombre2} est : {somme}")
