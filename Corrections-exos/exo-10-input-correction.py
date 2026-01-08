019bd385519cb798874ec32613d172c92980db9c
jonathan YHWH <alphajorider@gmail.com>
2026-01-08 13:11:48 +0100

    Update exo-10-input-correction.py

--- Corrections-exos/exo-10-input-correction.py
+++ Corrections-exos/exo-10-input-correction.py
@@ -5,3 +5,11 @@ Created on Thu Jan  8 13:05:15 2026
 @author: alpha
 """
 
+mot = input("Entrez un mot : \n")
+
+lettre_a_verifier =  "a"
+
+if lettre_a_verifier in mot:
+    print(f"La lettre '{lettre_a_verifier}' est présente dans le '{mot}'.")
+else:
+    print(f"La lettre '{lettre_a_verifier}' n'est pas présente dans le '{mot}'.")
